781b9dfc5508f29e044030b65261a446c2cea8ae
leodg021 <leodg_021@hotmail.com>
2026-02-05 14:00:31 -0700

    Streamlined REPL environment.

--- main.go
+++ main.go
@@ -8,6 +8,19 @@ import (
 	"github.com/leodg021/monkey/repl"
 )
 
+const MONKEY_FACE = `
+  .--.  .-"     "-.  .--.
+ / .. \/  .-. .-.  \/ .. \
+| |  '|  /   Y   \  |'  | |
+| \   \  \ 0 | 0 /  /   / |
+ \ '- ,\.-"""""""-./, -' /
+  ''-' /_   ^ ^   _\ '-''
+      |  \._   _./  |
+      \   \ '~' /   /
+       '._ '-=-' _.'
+          '-----'
+`
+
 func main() {
 	user, err := user.Current()
 	if err != nil {
@@ -15,6 +28,7 @@ func main() {
 	}
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
 		user.Username)
+	fmt.Printf(MONKEY_FACE)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
 }
--- repl/repl.go
+++ repl/repl.go
@@ -6,10 +6,32 @@ import (
 	"io"
 
 	"github.com/leodg021/monkey/lexer"
-	"github.com/leodg021/monkey/token"
+	"github.com/leodg021/monkey/parser"
 )
 
-const PROMPT = ">>"
+const PROMPT = ">> "
+
+const SAD_MONKEY_FACE = `
+  .--.  .-"     "-.  .--.
+ / .. \/  .-. .-.  \/ .. \
+| |  '|  /   Y   \  |'  | |
+| \   \  \ x | x /  /   / |
+ \ '- ,\.-"""""""-./, -' /
+  ''-' /_   ^ ^   _\ '-''
+      |  \._   _./  |
+      \   \ '~' /   /
+       '._ '-=-' _.'
+          '-----'
+`
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, SAD_MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
+	}
+}
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -23,9 +45,15 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+		
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
 	}
 }
